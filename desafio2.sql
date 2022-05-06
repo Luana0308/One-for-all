9764c7a60528f852fc0aac5f67e6a022e5574858
Luana <luanaff-2008@hotmail.com>
2022-05-06 16:08:26 -0300

    requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+SELECT 
+    COUNT(cancoes.cancao) AS cancoes, 
+    COUNT(DISTINCT artistas.artista) AS artistas, 
+    COUNT(DISTINCT albuns.album) AS albuns 
+FROM SpotifyClone.cancoes
+INNER JOIN SpotifyClone.album AS albuns  ON albuns.album_id = cancoes.album_id
+INNER JOIN SpotifyClone.artista AS artistas ON artistas.artista_id = albuns.artista_id;
